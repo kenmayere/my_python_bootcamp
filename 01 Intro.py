ef2e06d39e0a673490b35f9189db7fd4833099de
kenmayere <126105624+kenmayere@users.noreply.github.com>
2026-07-13 14:18:41 +0000

    Added code

--- 01 Intro.py
+++ 01 Intro.py
@@ -47,6 +47,8 @@ print(string_with_quotes) # OR escaping below
 
 another_string_with_quotes = 'He said \'I will go\'' # The backslash escape the quotes characters/remove meaning
 print(another_string_with_quotes)
+let_me_try = 'I am trying it again with this word in quotes \'Try\''
+print(let_me_try) # I can now use this syntax \' text \' to escape characters.
 
 # Finally I can join string characters using '+' sign
 
@@ -67,11 +69,16 @@ casting or changing the data structure, I can use f-strings'''
 
 price = 1000
 print(f'The price for the laptop is {price} dollars')
+# It is important to note the format which is: f'text1{integer}text2..,'
+
+# Another example:
+shoe_size = 7
+print(f'Please get me size {shoe_size} of any color')
 
 '''However, the limitation with f-strings is that I cannot reuse them when the value
 of the variable changes, for example'''
 
-price = 2000
+price = 4000
 print(f'The new price of the laptop is {price} dollars') # Oh it has worked
 
 # Let me try something called nesting
@@ -81,12 +88,14 @@ for example, I ask users their input'''
 
 user_input = input('What is your age: ') # I enter 3 for example
 print(f'You have lived for {user_input * 12} months') # Here it will concatenate the 3's 12 times
+print(type(user_input)) # If I try to check the type: <class 'str'> thats why it concatenate
 
 ''' If I want to perform mathematical operations
 I need to cast the user input as an integer. I can do that under the same line by nesting'''
 
 user_input = int(input('What is your age: ')) # I have nested two functions, int and input
 print(f'You have lived for {user_input * 365 * 24 * 60 * 60} seconds on earth')
+print(type(user_input)) # Here it gives <class 'int'> because I casted it to int in the nested function
 
 # Working with booleans
 # They evaluate to True or False using comparison operators (==, >=, <=, =!)
@@ -137,18 +146,23 @@ birth_month = [['Ken', 'Dec'], ['Yammie', 'Oct'], ['Mpha', 'Sept']]
 However, it is best practice to wrap tuples in round brackets'''
 
 tuple_example = 'Ken', 'Yammie', 'Mpha'
-print(type(tuple_example)) # <class 'tuple'>
+print(type(tuple_example)) # Here the result will be <class 'tuple'>
 
 another_tuple = ('Ken', 'Yammie', 'Mpha')
 print(type(another_tuple)) # <class 'tuple'>
 
-# The major difference with lists is that why lists can be altered (add/remove), tuples do not change
+''' Can I then access elements in a tuple the way I would with a list?
+Lets try!
+'''
+print(tuple_example[0]) # Yes it works
+
+# The major difference with lists is that lists can be altered (add/remove), tuples do not change
 
 # tuple_example.append('Amina') # Will result into AttributeError: 'tuple' object has no attribute 'append'
 
 # I can only join a tuple to a tuple
 
-tuple_example = tuple_example + ('Amina',)
+tuple_example = tuple_example + ('Amina',) # Here it is important to note the added sep (,) to make it work
 print(tuple_example) # It will result into ('Ken', 'Yammie', 'Mpha', 'Amina')
 
 
@@ -162,3 +176,55 @@ print(randon_num) # {17, 34, 20, 56, 9}
 # Can I add to a set, let me try
 randon_num.add(23)
 print(randon_num) # It has worked
+
+# Do sets contain strings, lets see:
+string_set = {'Kenford','Yammie','Mphatso','Amina'}
+print(string_set) # It works perfectly
+
+# What about set methods, let me try the common methods. add/remove/append
+
+string_set.add('Lai')
+print(string_set)
+
+# Let me pop
+string_set.pop() # It will delete the first element at any given time
+print(string_set)
+
+# But why are sets important over tuples and lists
+'''With sets I can perform advanced functions
+for example:
+    difference = elements that are in one set but not in another
+    symmetric difference = elements that are not in both
+    intersection = elements that are in both
+    union = one long set without duplicates
+'''
+
+# For example
+saprono = {'Eluphy','Tio','Love','Tindi','Fai','Aggie'}
+alto = {'Aggie','Thembi','Yammie','Fai','Tindi'}
+
+not_saprono = alto.difference(saprono) # Elements in alto that are not in sap
+print(not_saprono)
+not_alto = saprono.difference(alto) # Elements in sap but not in alto
+print(not_alto)
+
+one_voice = saprono.symmetric_difference(alto) # Elements that are only in sap
+print(one_voice)
+
+sap_or_alto = saprono.intersection(alto) # Elements appearing in both
+print(sap_or_alto)
+
+print(saprono.union(alto)) # One set without duplicates
+
+'''Dictionary'''
+
+'''It allows one to store key:value pairs
+These are wrapped inside curly braces
+You can assign a value by defining a key or change a value of a key to an existing key'''
+
+dict_one = {'name':'Ken', 'voice':'Tenor'}
+print(dict_one['name']) # Calling out a value by the key
+
+# What is I want to store such information for a group: I can create a tuple of dictionaries
+
+choir = ({'name':'Ken', 'voice':'Tenor'}, {'name':'JD', 'voice':'Tenor'})
