00a0877d7897a016373575b08c8aa2b0518c87a6
kenmayere <126105624+kenmayere@users.noreply.github.com>
2026-07-15 11:53:35 +0000

    Understanding Fundermentals

---
+++ 02 Fundermentals.py
@@ -0,0 +1,11 @@
+''' This follows the introduction section of the python bootcamp'''
+
+# If statements
+
+''' These by definition evaluates a bool and run a block of code if its true basically'''
+
+name = 'Rolf'
+user_name = input('Please enter your name: ')
+
+if user_name == name:
+    print('Hello friend!')
