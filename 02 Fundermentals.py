1603a8cbe966a73492846246236417f45ff781be
kenmayere <126105624+kenmayere@users.noreply.github.com>
2026-07-20 14:41:47 +0000

    Added destructuring of syntax and iterating over dictionaries

--- 02 Fundermentals.py
+++ 02 Fundermentals.py
@@ -76,6 +76,39 @@ for results in students:
     grade = results['grade']
     print(f'{name} has got {grade} marks')
     
-''' Agin here, the for loop create a variable results and assign it the first value in the list which is 
-a dictionary, then we assign the first value in the dict with a key called name to name variable,
-similary grade to grade variable using the key grade. For each iteration, the print function is running'''
+''' Agin here, the for loop create a variable 'results' and assign it the first value in the list which is 
+a dictionary, then we assign the first value in the dict with a key called name to 'name' variable,
+similary grade to 'grade' variable using the key grade. For each iteration, the print function is running'''
+
+# Now let's talk about destructuring syntax
+
+# Here is an example: Suppose I have a tuple of currencies
+currencies = 0.8, 1.2
+usd, eur = currencies # Here I have assigned first element in the tuple to usd and the other to eur
+# This is called destructuring
+# A more concrete example
+
+players = [('Rolf', 25), ('Anne', 37), ('Charlie', 31), ('Bob', 22)] # A list of tuples with name and points.
+
+# To print out the elements, I can use a for loop:
+for player in players:
+    print(player)
+# But it will just print out the tuples, to make it more intuitive, I can destricture it:
+
+for name, points in players:
+    print(f'{name} has {points} points this season')
+
+# What about iterating over a dictionary
+# Suppose the player plus points data was a dict
+
+player_points = dict(players) # Here I have converted the list to a dict with the tuples as key:value pairs
+
+for player in player_points:
+    print(player)
+# Here I will only get a print out of all the keys which are the names of the players in this case
+# But if I want to print out the values which are the player points in this case;
+for points in player_points.values():
+    print(points)
+# But I want to make it more intuitive and print out both key and value
+for name, points in player_points.items():
+    print(f'{name} has {points} points this season')
