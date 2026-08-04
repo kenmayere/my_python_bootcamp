809ffd10b91fd5d1459ccf2e48af7ef576aa592d
kenmayere <126105624+kenmayere@users.noreply.github.com>
2026-08-04 11:29:00 +0000

    Added brief, notes and hints to follow when coding

--- 03 Project 1.py
+++ 03 Project 1.py
@@ -1,1 +1,28 @@
+### ----------------------------------- A Movie Collection App ------------------------------------ ###
+
 '''This is a milestone project, I will go over the brief, try it out and then follow through the solutions for evaluation'''
+
+# I must admit that I really liked Jose's brief of the project because of its logic
+# I learnt alot of how I need to break down problems with code-thinking or logic
+# I will now highlight major steps and I will code my solution, making use of the provided hints as well
+
+'''
+First, I need to create user stories in the form of 'As a user, I would like to be able to.. '
+    a. Add new movies to my collection; So I can keep track of all my movies
+    b. List all the movies in my collection; So I can see what movies I already have
+    c. Find a movie by using the movie title; So I can locate a specific movie easily when the collection grows
+
+Implementation Tasks
+    - Decide where to store the movies in code
+    - Decide what data I want to store for each movie
+    - Show the user a menu and let them pick an option
+    - Implement each requirement in turn, each as a seperate function
+    - Stop running the prog. when user type in 'q' in the menu
+
+Breakdown:
+    1. Where to store the movies: I will use a list, it was hinted as well to be easy to add more information
+    2. What data to store: Title, director, year of release. This will be in a dict
+    3. Show user a menu, get input and run a loop
+
+'''
+
