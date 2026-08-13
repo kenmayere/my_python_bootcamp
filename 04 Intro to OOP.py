aca79fa97c39aea690e485c6584eef2fb2a0c114
kenmayere <126105624+kenmayere@users.noreply.github.com>
2026-08-13 17:13:56 +0000

    Discussed classes and methods

--- 04 Intro to OOP.py
+++ 04 Intro to OOP.py
@@ -2,7 +2,6 @@
 ------------------- Intro to Object Oriented Programming --------------------
 '''
 # I start by creating a student library to contain name and a list of grades
-
 my_student = {
 'name': 'Rolf Smith',
 'grades': [90, 88, 90, 99]
@@ -33,15 +32,46 @@ class Student:
     def average(self):
         return sum(self.grades)/len(self.grades)
 
-# This has defined the structure of the object. A few things to note
-# 1. The object is created using a 'class' key word
-# 2. The object name 'Student' starts with an upper class
+''' ------------ A few points here: ----------------
+1. The object is created using a 'class' key word
+2. The object name 'Student' starts with an upper class
+'''
 
 # Step 2: Creating the object >> This essentially means calling the object structure
-student_one = Student()
+student_one = Student('Ken', [70, 80, 90, 99])
+student_two = Student('Ford', [75, 80, 85, 99])
 
-# Here are a few points
-# 1. student_one is the object that we have created using the object structure 'Student'
-#     - I should think of objects as holding both data and actions
-# 2. When it (Student) is created, it immediately calls the dunder init function which pass the self argument
-# 3. This self argument creates a blank object, which eventually populates using the 'self.' for name and grades
+''' ------------ A few points here: ----------------
+
+1. Student_one is the object that we have created using the object structure 'Student'
+     - I should think of objects as holding both data and actions
+2. When it (Student) is created, it immediately calls the dunder init function which pass the self argument
+3. This self argument creates a blank object, which eventually populates using the 'self.' for name and grades
+4. Now student_one and _two are objects that contain the data (name and grades passed to it).
+    - These are called properties. Normally, they could have been called variables
+2. The objects also have a function inside it. Functions inside a class are called Methods
+    - The functions takes in an argument 'self' which is the now populated object
+    - To call the function, it uses the syntax Object.Method_name()
+'''
+# Calling the method
+print(student_one.average())
+# In the background python is running Student.average(student_one)
+# Thus accessing the class, then the method, finally passing the object
+# But, Object.method() already position 'student_one' as the self aurgument in the class structure
+# Which means, we can add more parameters in the call, but will point to other properties in the object
+
+
+# Test: Creating class for a movie dictionary
+movie = {
+    'name': 'Blackberry',
+    'director': 'Matth Johnson'
+}
+# Creating a class to create a 'Movie' object
+class Movie:
+    def __init__(self, movie_name, movie_director):
+        self.name = movie_name
+        self.director = movie_director
+
+# Creating the Movie Object to contain the data in the movie dictionary
+movie_object = Movie('Blackberry', 'Matt Johnson')
+
